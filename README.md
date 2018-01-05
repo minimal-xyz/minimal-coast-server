f4a3c859d61d51aa25992ca0d4e36e85cc291647
ChenYong <jiyinyiyong@dingtalk.com>
2018-01-05 12:08:59 +0800

    commit minimal example

---
+++ README.md
@@ -0,0 +1,13 @@
+
+Minimal Clojure Web Server example in Coast
+----
+
+To run it:
+
+```bash
+clj -m app.main
+```
+
+### License
+
+MIT
---
+++ src/app/main.cljs
@@ -0,0 +1,13 @@
+
+(ns app.main
+  (:require [coast.core :as coast]))
+
+(defn home [request]
+  (coast/ok "Hello World"))
+
+(def app (-> (coast/get "/" home)
+             (coast/match-routes)))
+
+(defn -main []
+  (coast/start-server app {:port 3000})
+  (println "listening on 3000..."))
